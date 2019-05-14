969308a8e440c9707b07c20a7a735a576424c79a
SuleymaniOnNet <suleymani.2ral@gmail.com>
2019-05-14 11:59:06 +0400

    bankofbaku provider implemented

--- InReflectionWeTrust/BankOfBank/BankOfBakuProvider.cs
+++ InReflectionWeTrust/BankOfBank/BankOfBakuProvider.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using ProviderProtocol;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BankOfBank
 {
-    public class BankOfBakuProvider
+    public class BankOfBakuProvider : IProvider
     {
+        public string Name { get; set; } = "Bank Of Baku";
+
+        public void OnButtonClicked()
+        {
+           //implementation simplified for learning..
+            MessageBox.Show("This is Bank of Baku provider");
+        }
     }
 }
