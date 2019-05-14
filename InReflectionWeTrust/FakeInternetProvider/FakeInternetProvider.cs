948eba28f5e0897ad2ed2b0fcae299f92b2b53a6
SuleymaniOnNet <suleymani.2ral@gmail.com>
2019-05-14 13:26:07 +0400

    fake internet provider added

---
+++ InReflectionWeTrust/FakeInternetProvider/FakeInternetProvider.cs
@@ -0,0 +1,20 @@
+﻿using ProviderProtocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FakeInternetProvider
+{
+    public class FakeInternetProvider : IProvider
+    {
+        public string Name { get; set; } = "Fake Internet Provider";
+
+        public void OnButtonClicked()
+        {
+            MessageBox.Show("This is fake provider");
+        }
+    }
+}
